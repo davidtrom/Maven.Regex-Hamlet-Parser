957fc920e213ba18ffca3f01c3ed9e02ecb677c9
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-11-22 20:01:46 -0500

    all tests passing with methods created

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,9 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.*;
+
 
 /**
  * Created by thook on 10/7/15.
@@ -8,14 +11,16 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    private Pattern pattern;
+    private Matcher matcher;
 
-    public HamletParser(){
+    public HamletParser() throws FileNotFoundException {
         this.hamletData = loadFile();
     }
 
     private String loadFile(){
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("hamlet.txt").getFile());
+        //ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File("/Users/david/Desktop/TrombelloProjects/IntelliJ Labs/Maven.Regex-Hamlet-Parser/src/main/resources/hamlet.txt");
         StringBuilder result = new StringBuilder("");
 
         try(Scanner scanner = new Scanner(file)){
@@ -32,8 +37,41 @@ public class HamletParser {
         return result.toString();
     }
 
+//    private String myFileLoader () throws FileNotFoundException {
+//        StringBuilder sb = new StringBuilder("");
+//        File file = new File("/Users/david/Desktop/TrombelloProjects/IntelliJ Labs/Maven.Regex-Hamlet-Parser/src/main/resources/hamlet.txt");
+//        Scanner scanner = new Scanner(file);
+//            while(scanner.hasNextLine()){
+//                String line = scanner.nextLine();
+//                sb.append(line).append("\n");
+//            }
+//        return sb.toString();
+//    }
+
+
     public String getHamletData(){
         return hamletData;
+    }
+
+    public void findHamlet () throws FileNotFoundException {
+        System.out.println(hamletData);
+    }
+
+    public String replaceHamlet () {
+        String regex = "Hamlet";
+        pattern = Pattern.compile(regex);   //pattern to look for in the data
+        matcher = pattern.matcher(hamletData);  //matches the given pattern in the data
+        String newHamlet  =matcher.replaceAll(("Leon"));    //the matches get replaced with the new word in parenthesis
+        return newHamlet;
+        //System.out.println(newHamlet);
+    }
+
+    public String replaceHoratio () {
+        String regex = "Horatio";
+        pattern = Pattern.compile(regex);   //pattern to look for in the data
+        matcher = pattern.matcher(hamletData);  //matches the given pattern in the data
+        String newHamlet  =matcher.replaceAll(("Tariq"));    //the matches get replaced with the new word in parenthesis
+        return newHamlet;
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,10 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -8,24 +13,52 @@ public class HamletParserTest {
     private HamletParser hamletParser;
 
     @Before
-    public void setUp() {
+    public void setUp() throws FileNotFoundException {
         this.hamletParser = new HamletParser();
         this.hamletText = hamletParser.getHamletData();
     }
 
     @Test
     public void testChangeHamletToLeon() {
+        Boolean expected = false;
+        String noHamlet = hamletParser.replaceHamlet();
+        Boolean actual = noHamlet.contains("Hamlet");
+        Assert.assertEquals(expected, actual);
+        //Pattern pattern = Pattern.compile("Leon");
+        //Matcher matcher = pattern.matcher(hamletData);
+        //Boolean actual = matcher.matches(regex);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        Boolean expected = false;
+        String noHamlet = hamletParser.replaceHamlet();
+        Boolean actual = noHamlet.contains("Hamlet");
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
+        Boolean expected = true;
+        Boolean actual = hamletText.contains("Horatio");
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHamlet() {
+        Boolean expected = true;
+        Boolean actual = hamletText.contains("Hamlet");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findFile () throws FileNotFoundException {
+        hamletParser.findHamlet();
+    }
+
+    @Test
+    public void replaceHamlet () {
+        hamletParser.replaceHamlet();
     }
 }
